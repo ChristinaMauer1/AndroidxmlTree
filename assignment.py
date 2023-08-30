0a4efcc4a8d83b94b9d14811b8a2e0ef65751430
ChristinaMauer1 <143666135+ChristinaMauer1@users.noreply.github.com>
2023-08-30 19:46:39 -0400

    Creating and testing tree aspect of assignment
    In Python 3.11

---
+++ assignment.py
@@ -0,0 +1,20 @@
+class CompNode:
+    def __init__(self, bound):
+        self.children = []
+        self.boundaries = bound
+
+    def addChild(node):
+        self.children.append(node)
+    def getChildren():
+        return children
+
+
+root = CompNode(0)
+m = CompNode(1)
+root.children.append(m)
+m = CompNode(2)
+root.children.append(m)
+m = CompNode(3)
+root.children.append(m)
+for i in root.children:
+    print(i.boundaries)
